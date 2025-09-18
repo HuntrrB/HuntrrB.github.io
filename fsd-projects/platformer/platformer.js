ad676d54068c1b4ccc85c86d8ce3d57cf8d0540b
HuntrrB <bh790648@gmail.com>
2025-09-18 23:58:39 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,69 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //toggleGrid();
+    // Might disable this cuz it lowkey kinda lags my laptop....
 
 
     // TODO 2 - Create Platforms
+    //Walls will be black I think. Platforms red.
+    //What i am aiming for this first part is a zig zag type jump.
+
+    //PLATFORMS GROUP #1
+
+    createPlatform(250, 605, 85, 20, "red"); //First starting platform. You will have to do some weird jumps to get up.
+
+    createPlatform(95, 505, 85, 20, "red"); //First starting platform. You will have to do some weird jumps to get up.
+      
+    createPlatform(250, 405, 85, 20, "red"); //First starting platform. You will have to do some weird jumps to get up.
+
+    //Platform group #2
+    //These'll be blue this time.
+    createPlatform(1330, 607, 85, 20, "blue"); //First starting platform. You will have to do some weird jumps to get up.
+
+    createPlatform(1200, 505, 85, 20, "blue"); //First starting platform. You will have to do some weird jumps to get up.
+      
+    createPlatform(1330, 405, 85, 20, "blue"); //First starting platform. You will have to do some weird jumps to get up.
+    //top half
+
+    createPlatform(1200, 303, 85, 20, "blue"); //First starting platform. You will have to do some weird jumps to get up.
+      
+
+    //WALLS
+
+    createPlatform(300, 305, 50, 500, "black"); //wall test //Wall #1
+
+    createPlatform(500, 0, 50, 500, "black"); //wall test //Wall #2 
+
+    //Next, make a horizontal roof for the second wall. It will go on the side. This will create a mini forced cave.
 
 
 
 
     // TODO 3 - Create Collectables
+    createCollectable("database", 400, 700);
+      
+    createCollectable("steve", 350, 500);
+
+    createCollectable("diamond", 450, 200);
+
 
 
 
     
     // TODO 4 - Create Cannons
+
+    createCannon("top", 500, 2000); //This cannon lies on top of the screen and shoots down.
+
+    createCannon("bottom", 600, 900); //Shoots from da bottom.
+
+    createCannon("bottom", 800, 800); //Shoots from da bottom.
+
+    createCannon("bottom", 1000, 900); //Shoots from da bottom.
+
+    //The EVIL cannon that is in between the platforms on the rightmost side of the screen that you jump on. It is EVIL because it adds a horrible challenge.
+    
+    createCannon("bottom", 1250, 2500); //Shoots from da bottom.
 
 
     
