3c09ecdef2b29501834e5662cb96600c4e8edca0
HuntrrB <bh790648@gmail.com>
2026-01-30 01:46:01 +0000

    saving data shapes progress

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -34,16 +34,46 @@ $(document).ready(function () {
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
 
+  // TODO 0 complete. Okay.
+
   // TODO 1: create a new shape object and add it to the array
-  
+  var shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3,
+  };
+
+  dataShapes.push(shape); //VERY useful for adding items to a list. This makes it go-
+  //to the end of the list. 
 
   // TODO 2: add a new property to all data shapes
+  for (var i = 0; i < dataShapes.length; i++ ) {
+   let currentShape = dataShapes[i]; //Okay this actually works and doesn't make the program scream. 
+    
+    if (currentShape.color == "red") {
+      currentShape.goodBehavior = "bounce";
+    }
+      else if (currentShape.color == "blue") {
+      currentShape.goodBehavior = "blink";
+    }
+      else {
+      currentShape.goodBehavior = "spin";
+    };
+  };
   
 
   // TODO 3-a: add a function that handles the static display type
   
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1;
+  };
 
   // TODO 4-a: add a function that handles the good display type
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+  }
   
 
   // TODO 5-a: add a function that handles the bad display type
@@ -55,13 +85,15 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
-  }
+    var currentShape = dataShapes[currentIndex];
+    handleStatic(currentShape);
+  };
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
-  }
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape);
+  };
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
