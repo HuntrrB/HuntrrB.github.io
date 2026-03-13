82a26e1263576fa6699783aef316a7f0c0741a0e
HuntrrB <bh790648@gmail.com>
2026-03-13 00:54:58 +0000

    Completed walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,6 +10,25 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+
+  //Global walker var.
+
+  var walker = {
+    X: 0,
+    Y: 0,
+    speedX: 0,
+    speedY: 0,
+  };
+
+  //Key stuf
+
+  const KEY = {
+    ENTER: 13,
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+  };
   
   // Game Item Objects
 
@@ -23,7 +42,8 @@ function runProgram(){
 
   Note: You can have multiple event listeners for different types of events.
   */
-  $(document).on('eventType', handleEvent);                          
+$(document).on('keydown', handleKeyDown);          
+$(document).on('keyup', handleKeyUp);                        
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,7 +54,9 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem();
+    redrawGameItem();
+    wallCollision();
 
   }
   
@@ -44,8 +66,53 @@ function runProgram(){
   
   Note: You can have multiple event handlers for different types of events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+    console.log(event.which);
 
+    if (event.which === KEY.LEFT) {
+      walker.speedX = -5;
+      return;
+      // console.log("left key pressed");
+    }
+
+     if (event.which === KEY.RIGHT) {
+      walker.speedX = 5;
+      // console.log("right key pressed");
+    }
+
+     if (event.which === KEY.UP) {
+      walker.speedY = -5;
+      // console.log("up key pressed");
+    }
+
+     if (event.which === KEY.DOWN) {
+      walker.speedY = 5;
+      // console.log("down key pressed");
+    }
+
+  }
+
+  function handleKeyUp(event) {
+
+    if (event.which === KEY.LEFT) {
+      walker.speedX = 0;
+      // console.log("left key pressed");
+    }
+
+     if (event.which === KEY.RIGHT) {
+      walker.speedX = 0;
+      // console.log("right key pressed");
+    }
+
+     if (event.which === KEY.UP) {
+      walker.speedY = 0;
+      // console.log("up key pressed");
+    }
+
+     if (event.which === KEY.DOWN) {
+      walker.speedY = 0;
+      // console.log("down key pressed");
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -59,6 +126,40 @@ function runProgram(){
 
     // turn off event handlers
     $(document).off();
+  }
+
+  //Updating the walker POS.
+
+  function repositionGameItem() {
+    walker.X = walker.speedX + walker.X;
+    walker.Y = walker.speedY + walker.Y;
+  }
+
+  //Drawing the walker to the correct location.
+
+  function redrawGameItem() {
+    $("#walker").css("left", walker.X);
+  $("#walker").css("top", walker.Y);
+  }
+
+  //Wall collision.
+
+  function wallCollision() {
+    if (walker.X < 0) {
+      walker.X -= walker.speedX;
+    }
+
+     if (walker.X > $("#board").width() - 55) {
+      walker.X -= walker.speedX;
+    }
+
+     if (walker.Y < 0) {
+      walker.Y -= walker.speedY;
+    }
+
+    if (walker.Y > $("#board").height() - 55) {
+      walker.Y -= walker.speedY;
+    }
   }
   
 }
